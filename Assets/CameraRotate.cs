d54defaf9b4f0d462dd9c9e7d4d755988fce76dd
ryou <visualjupiter@gmail.com>
2018-04-17 13:42:30 +0900

    カメラ操作機能ざっと実装

--- Assets/CameraRotate.cs
+++ Assets/CameraRotate.cs
@@ -2,31 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour {
-
+public class CameraRotate : MonoBehaviour {
     private Vector3 lastMousePosition;
+    private Vector3 totalMovePosition = Vector3.zero;
 
     private GameObject mainCamera;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         this.mainCamera = this.transform.Find("Main Camera").gameObject;
     }
 
     // Update is called once per frame
-    void Update () {
-        if (Input.GetMouseButtonDown(1))
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
             var mousePosition = Input.mousePosition;
             this.lastMousePosition = mousePosition;
         }
-        else if (Input.GetMouseButton(1))
+        else if (Input.GetMouseButton(0))
         {
             var mousePosition = Input.mousePosition;
             var mouseMove = mousePosition - this.lastMousePosition;
-            var move = mouseMove * -0.01f;
+            this.totalMovePosition += mouseMove;
+            var angleMove = new Vector3(this.totalMovePosition.y, this.totalMovePosition.x, 0);
 
-            this.mainCamera.transform.Translate(move.x, move.y, 0);
+            this.transform.rotation = Quaternion.Euler(angleMove);
 
             this.lastMousePosition = mousePosition;
         }
---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+    private GameObject mainCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.mainCamera = this.transform.Find("Main Camera").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        this.mainCamera.transform.Translate(0, 0, scroll);
+    }
+}
